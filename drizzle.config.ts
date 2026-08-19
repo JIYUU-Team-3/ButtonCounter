0f1825012b29309f34d837c4be58c508df87e7c3
Uteytithya <tithyautey@gmail.com>
2026-08-19 10:28:09 +0700

    feat: add npm run generate to run schema migration to db (#3)

---
+++ drizzle.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'drizzle-kit';
+
+import dotenv from "dotenv";
+
+dotenv.config({ path: ".env.local" });
+
+export default defineConfig({
+  dialect: 'turso',
+  schema: './migrations/schema.ts',
+  out: './drizzle',
+  dbCredentials: {
+    url: process.env.TURSO_DATABASE_URL!,
+    authToken: process.env.TURSO_AUTH_TOKEN!,
+  },
+});   
