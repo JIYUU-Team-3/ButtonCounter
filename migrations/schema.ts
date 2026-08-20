05c42df0323e4dbce17ac3beea531876b6dba9ec
Uteytithya <tithyautey@gmail.com>
2026-08-20 14:59:25 +0700

    fix: change function to use sqlite-core function for table creation (#4)

--- migrations/schema.ts
+++ migrations/schema.ts
@@ -1,8 +1,8 @@
-import * as p from 'drizzle-orm/pg-core';
+import * as p from 'drizzle-orm/sqlite-core';
 
-export const counts = p.pgTable('count', {
-	id: p.serial().primaryKey(),
-	count: p.integer()
+export const counts = p.sqliteTable('counts', {
+	id: p.integer().primaryKey({ autoIncrement: true }),
+	count: p.integer().notNull().default(0)
 });
 
 export type InsertCount = typeof counts.$inferInsert;
