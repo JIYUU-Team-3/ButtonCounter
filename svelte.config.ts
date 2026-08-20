4b00c95d2c81fdbcfab4923648973fa7e4fcb51b
Uteytithya <tithyautey@gmail.com>
2026-08-20 14:58:31 +0700

    build: added experimental feature query and command (#4)

--- svelte.config.ts
+++
@@ -1,7 +0,0 @@
-import adapter from 'svelte-adapter-uws';
-
-export default {
-  kit: {
-    adapter: adapter({ websocket: true })
-  }
-};
--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import adapter from '@sveltejs/adapter-node';
 import { sveltekit } from '@sveltejs/kit/vite';
 import uws from 'svelte-adapter-uws/vite';
 import realtime from 'svelte-realtime/vite';
+/** @type {import('@sveltejs/kit').Config} */
 
 export default defineConfig({
 	plugins: [
@@ -13,7 +14,13 @@ export default defineConfig({
 		uws(),
 		realtime(),
 		sveltekit({
+			experimental: {
+				remoteFunctions: true
+			},
 			compilerOptions: {
+				experimental: {
+					async: true
+				},
 				// Force runes mode for the project, except for libraries. Can be removed in svelte 6.
 				runes: ({ filename }) =>
 					filename.split(/[/\\]/).includes('node_modules') ? undefined : true
