48962c0b866edbfa75fa08a89f16df6184a758e0
Uteytithya <tithyautey@gmail.com>
2026-08-20 14:58:31 +0700

    build: added experimental feature query and command (#4)

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,12 +2,12 @@ import { defineConfig } from 'drizzle-kit';
 
 import dotenv from 'dotenv';
 
-dotenv.config({ path: '.env.local' });
+dotenv.config({ path: '.env' });
 
 export default defineConfig({
 	dialect: 'turso',
 	schema: './migrations/schema.ts',
-	out: './drizzle',
+	out: './migrations-folder',
 	dbCredentials: {
 		url: process.env.TURSO_DATABASE_URL!,
 		authToken: process.env.TURSO_AUTH_TOKEN!
